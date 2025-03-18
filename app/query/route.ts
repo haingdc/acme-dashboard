9d80f53e80390cb0df408aa91ae885d1f92ccb06
Hai.Nguyen <haingdc@gmail.com>
2025-03-18 15:45:04 +0700

    Update database connection to use hardcoded Neon database URL for testing

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,7 +1,9 @@
 import { neon } from '@neondatabase/serverless';
-import process from 'node:process'
+// import process from 'node:process'
 
-const sql = neon(process.env.POSTGRES_URL!);
+
+// const sql = neon(process.env.POSTGRES_URL!);
+const sql = neon("postgres://neondb_owner:npg_iXpeAgQLrY74@ep-nameless-recipe-a168bzgy-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require");
 
 async function listInvoices() {
 	const data = await sql`
