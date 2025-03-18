d58f1b4be36f5f7ac2940cdbc8f1b32514851667
Hai.Nguyen <haingdc@gmail.com>
2025-03-18 15:11:15 +0700

    Add route for listing invoices and integrate @neondatabase/serverless

---
+++ app/query/route.ts
@@ -0,0 +1,23 @@
+import { neon } from '@neondatabase/serverless';
+
+const databaseUrl = Deno.env.get('DATABASE_URL')!;
+const sql = neon(databaseUrl);
+
+async function listInvoices() {
+	const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
+
+	return data;
+}
+
+export async function GET() {
+  try {
+  	return Response.json(await listInvoices());
+  } catch (error) {
+  	return Response.json({ error }, { status: 500 });
+  }
+}
