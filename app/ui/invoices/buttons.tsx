aa56d1199af31b78be0ce88995a0dee4063de288
Hai.Nguyen <haingdc@gmail.com>
2025-03-20 10:18:38 +0700

    chapter 12: thêm các tính năng sửa đổi invoices: + page add + page edit + nút xoá invoice

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,5 +1,6 @@
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { deleteInvoice } from '@/app/lib/actions.ts';
 
 export function CreateInvoice() {
   return (
@@ -13,11 +14,10 @@ export function CreateInvoice() {
   );
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href="/dashboard/invoices"
+      href={`/dashboard/invoices/${id}/edit`}
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
@@ -25,14 +25,14 @@ export function UpdateInvoice({ id }: { id: string }) {
   );
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
   return (
-    <>
+    <form action={deleteInvoiceWithId}>
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
       </button>
-    </>
+    </form>
   );
 }
